b874f08d2b9edf88eaf2852bc141487079e33849
BersUmar <umar-03@inbox.ru>
2023-12-05 10:24:31 +0300

    first migration

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+class Category(models.Model):
+    category = models.CharField(max_length=75, verbose_name='категория')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.category} - {self.description}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Product(models.Model):
+    title = models.CharField(max_length=75, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+    photo = models.ImageField(upload_to='catalog/',verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.PositiveIntegerField(verbose_name='цена за штуку')
+    date = models.DateField(verbose_name='дата создания')
+    fix_date = models.DateField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.title} - {self.price}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'postgres',
     }
 }
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
