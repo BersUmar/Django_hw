afabd6b869843faa228a64dc9cf1e3c0315ea7cc
BersUmar <umar-03@inbox.ru>
2023-12-08 07:50:32 +0300

    add fixtures (data.json)

--- catalog/models.py
+++ catalog/models.py
@@ -6,7 +6,6 @@ NULLABLE = {'blank': True, 'null': True}
 class Category(models.Model):
     category = models.CharField(max_length=75, verbose_name='категория')
     description = models.TextField(verbose_name='описание')
-
     def __str__(self):
         return f'{self.category} - {self.description}'
 
