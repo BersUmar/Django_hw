56dd1498dfeb9de4ee75729d5ce0b1c183918283
BersUmar <umar-03@inbox.ru>
2023-12-20 16:00:39 +0300

    change DataBase, settings and make new migrations

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,6 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
+        'USER': 'adamantius',
+        'PORT': 5430,
+        'HOST': '0.0.0.0',
+        'PASSWORD': '1313'
     }
 }
 
@@ -117,10 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIR = (
-    BASE_DIR / 'static'
-)
+# STATIC_URL = 'static/'
+
+# STATICFILES_DIR = (
+#     BASE_DIR / 'static'
+# )
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join (BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
